14cee8a92610c149f930bbd9e4fdf59dbf959554
thegna <thegna.praya.saravanan@gmail.com>
2025-03-11 20:50:58 +0530

    lab testing jenkins modify for task-05 test pipeline

---
+++ main/test.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout<<"I have modified this file for task-05 to test pipeline\n";
+  return 0;
+}
